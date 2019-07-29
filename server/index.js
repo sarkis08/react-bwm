135771ffb0d53c4406e597740d204e59bc45353a
sarkis08 <moriehenryk@gmail.com>
2019-07-29 17:38:22 +0000

    Add booking functionality, improve rentals and user routes

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ const Rental = require('./models/rental');
 
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/users');
+const bookingRoutes = require('./routes/bookings');
 
 mongoose.connect(config.DB_URL, { useNewUrlParser: true })
     .then(() => {
         const fakeDb = new FakeDb();
-        fakeDb.seedDb();
+        //fakeDb.seedDb();
         console.log('Connected to db');
     }).catch((err) => {
         console.log('Fail to connect to DB', err);
@@ -24,6 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/bookings', bookingRoutes);
 
 const port = process.env.PORT || 3001;
  
